2a0c9cf6fb2baa275b1e833d5d25ac072732af58
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-17 17:08:44 -0400

    setup post model

---
+++ db/migrate/20200917210158_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :body
+      t.references :author
+
+      t.timestamps
+    end
+
+    add_foreign_key :posts, :users, column: :author_id
+    
+  end
+end
