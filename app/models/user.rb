380fe6026d293db8f9c4f9db4dee64543bcbcce5
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-17 17:21:21 -0400

    added association user/post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
+  has_many :posts, class_name: "Post", foreign_key: "author_id"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
