335459235bd0e2c7ba2582f25ab7795a95ceb60c
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-19 19:45:27 -0400

    added bool choice between name/handle if handle is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_user_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_user_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :handle])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :handle])
+  end
 end
