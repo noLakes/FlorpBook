a0b48721b433ecbc890218c362e90f0661ae276b
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-22 19:35:39 -0400

    added methods to like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,19 @@ class LikesController < ApplicationController
 
   def create
     type = type_subject?(params)[0]
+    @subject = type_subject?(params)[1]
+    notice = "#{type}"
+    return unless @subject
+    if already_liked?(type)
+      dislike(type)
+    else
+      @like = @subject.likes.build(user_id: current_user.id)
+      if @like.save!
+        flash[:success] = "#{notice} liked!"
+
+      else
+        flash[:danger] = "#{notice} like failed!"
+    end
   end
 
   private
@@ -13,6 +26,29 @@ class LikesController < ApplicationController
     subject = Post.find(params[:post_id]) if type == 'post'
     subject = Comment.find(params[:comment_id]) if type == 'comment'
     [type, subject]
+  end
+
+  def already_liked?(type)
+    result = false
+    if type == 'post'
+      result = Like.where(user_id: current_user.id,
+                          post_id: params[:post_id]).exists?
+    end
+    if type == 'comment'
+      result = Like.where(user_id: current_user.id,
+                          comment_id: params[:comment_id]).exists?
+    end
+    result
+  end
+
+  def dislike(type)
+    @like = Like.find_by(post_id: params[:post_id]) if type == 
+                                                          'post'
+    @like = Like.find_by(comment_id: params[:comment_id]) if type == 
+                                                          'comment'
+    return unless @like
+
+    @like.destroy
   end
 
 end
