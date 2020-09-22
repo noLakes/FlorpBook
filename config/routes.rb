75dab07f1025d2574e463480df7d0a7efd651b67
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-21 21:13:09 -0400

    added nested likes route for comment and post

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users, only: [:show]
-  resources :posts
-  resources :comments
-  resources :likes, only: [:create]
+  
+  resources :posts do
+    resources :likes, only: [:create]
+  end
+
+  resources :comments do
+    resources :likes, only: [:create]
+  end
+  
   
   root "posts#index"
 end
