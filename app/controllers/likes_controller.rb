fff398e35a1de396806515107f4f77aab9977966
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-21 22:17:20 -0400

    working on like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,17 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    
+    type = type_subject?(params)[0]
+  end
+
+  private
+
+  def type_subject?(params)
+    type = 'post' if params.key?('post_id')
+    type = 'comment' if params.key?('comment_id')
+    subject = Post.find(params[:post_id]) if type == 'post'
+    subject = Comment.find(params[:comment_id]) if type == 'comment'
+    [type, subject]
   end
 
 end
