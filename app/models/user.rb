6d90cd873d2616e1965eb7bb8183d85c7ffa9ea2
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-22 20:40:27 -0400

    changed the name of sent/pending requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :comments, class_name: "Comment", foreign_key: "author_id"
   has_many :likes, dependent: :destroy
 
-  has_many :sent_friend_requests, class_name: "FriendRequest", foreign_key: "user_id"
-  has_many :pending_friend_requests, -> { where confirmed: false}, class_name: "FriendRequest",
+  has_many :sent_requests, class_name: "FriendRequest", foreign_key: "user_id"
+  has_many :pending_requests, -> { where confirmed: false}, class_name: "FriendRequest",
   foreign_key: "friend_id"
 
   devise :database_authenticatable, :registerable,
