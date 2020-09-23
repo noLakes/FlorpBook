a80d6b9d1e6efe2299f87243faa9a601ef41ced6
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-22 22:55:40 -0400

    added code for friendrequest model and user model friend queries

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def friends
+    friends_i_sent_inv = FriendRequest.where(user_id: id, confirmed: true).pluck(:id)
+    friends_i_received_inv = FriendRequest.where(friend_id: id, confirmed: true).pluck(:id)
+    ids = friends_i_sent_inv + friends_i_received_inv
+    User.where(id: ids)
+  end
+
+  def friend_with?(user)
+    FriendRequest.confirmed_record?(id, user.id)
+  end
+
+  def send_request(user)
+    sent_requests.create(friend_id: user.id)
+  end
+
 end
