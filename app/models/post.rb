b4180656a6cad7efaada61ac536cc1b274f45dcb
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-22 20:04:33 -0400

    added like count method to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments, class_name: "Comment", foreign_key: "post_id"
   has_many :likes, dependent: :destroy
+
+  def like_count
+    likes = self.likes.count
+    likes > 0 ? likes : nil
+  end
+
 end
