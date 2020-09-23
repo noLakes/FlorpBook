98568dd4019249974854b9951e3827f25e65c9a0
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-22 20:07:09 -0400

    added like count method to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: "Post"
   belongs_to :author, class_name: "User"
   has_many :likes, dependent: :destroy
+
+  def like_count
+    self.likes.count
+  end
+  
 end
