ea1d1b04142c8e12cfaadfd68c43efede52bb5c1
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-26 18:12:23 -0400

    added notification code after sending friend request

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -5,6 +5,11 @@ class FriendRequestsController < ApplicationController
   def create
     friend = User.find(params[:user_id])
     current_user.send_request(friend)
+    
+    @request = current_user.sent_requests.last
+    @notificaiton = new_notification(friend, @request.id, 'friend_request')
+    @notificaiton.save
+
     flash[:notice] = "Friend Request sent to #{friend.name}"
     redirect_back(fallback_location: root_path)
   end
