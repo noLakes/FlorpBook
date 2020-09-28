78a0f2b7605c784477c5bc4ef4c297ed869052a2
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-28 19:32:29 -0400

    added ability to dismiss notifications and delete them from record

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,8 +6,9 @@ class NotificationsController < ApplicationController
   end
 
   def destroy 
-    @notification = Notification.find(params[:notification_id])
+    @notification = Notification.find(params[:id])
     @notification.destroy
+    redirect_back(fallback_location: root_path)
   end
 
 end
