fcbd29f39f5396b6cd92c58bad43d20252c6e832
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-30 00:05:41 -0400

    added default pic method, with image file

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,6 +14,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url 
+    'default_pic.jpg'
+  end
+
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
