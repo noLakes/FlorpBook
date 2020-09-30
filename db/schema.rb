19ec8259a18f8c3ef309543fd543b6801b14a382
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-30 19:23:24 -0400

    added image uploader for posts, with migration for posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_230231) do
+ActiveRecord::Schema.define(version: 2020_09_30_231937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_230231) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
