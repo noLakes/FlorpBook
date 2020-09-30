a4498bb53cff67b341d4600d8654d8013d3fc8e0
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-29 23:58:57 -0400

    added feed size for user images

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,6 +33,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [100, 100]
   end
 
+  version :feed do
+    process resize_to_fit: [50, 50]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
