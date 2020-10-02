da052cd00326500e1fe0daaa0db9eff2cea1a97a
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-10-01 22:37:50 -0400

    added ENV variables for facebook-OAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,8 +273,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, "APP_ID", "APP_SECRET"
-  
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], token_params: { parse: :json }
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
