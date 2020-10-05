b7246c45d32e05ae21facd6ab958acb287e67325
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-10-05 18:10:47 -0400

    changed sendgrid settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,20 @@
-#for sendgrid
+#with api key
+#ActionMailer::Base.smtp_settings = {
+#  domain: "https://radiant-sea-37789.herokuapp.com/",
+#  address:        "smtp.sendgrid.net",
+#  port:            587,
+#  authentication: :plain,
+#  user_name:      'apikey',
+#  password:       ENV['SENDGRID_API_KEY']
+#}
+
+#without api key
 ActionMailer::Base.smtp_settings = {
-  domain: "https://radiant-sea-37789.herokuapp.com/",
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  password:       ENV['SENDGRID_API_KEY']
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  domain: 'https://radiant-sea-37789.herokuapp.com/',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
 }
